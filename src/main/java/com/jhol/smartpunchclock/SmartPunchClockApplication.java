97efbf7fe2acabc98567df45efc8cdccb082dfec
JhonatanRocha <jcristianrocha@gmail.com>
2021-01-04 21:00:54 -0300

    initial commit of project files

---
+++ src/main/java/com/jhol/smartpunchclock/SmartPunchClockApplication.java
@@ -0,0 +1,13 @@
+package com.jhol.smartpunchclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPunchClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPunchClockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhol/smartpunchclock/SmartPunchClockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhol.smartpunchclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPunchClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
